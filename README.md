43db3965b31e117bce577b0aff5ac3ff9e180ff1
Dmytro Prokhorenkov <liet@liet.kiev.ua>
2019-03-04 00:53:05 +0100

    Added timeout option

---
+++ README.md
@@ -0,0 +1,17 @@
+# icinga2_check_pihole
+
+Icinga2/Nagios plugin to check Pi-hole status.
+
+```bash
+usage: check_pihole.py [-h] [-H HOST] [-t TIMEOUT]
+
+Check for OOM killer events
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -H HOST, --host HOST  PiHole ip address or hostname
+  -t TIMEOUT, --timeout TIMEOUT
+                        Timeout for request
+
+check_pihole.py: v.0.1.1 by Dmytro Prokhorenkov
+```
--- check_pihole.py
+++ check_pihole.py
@@ -8,7 +8,7 @@ __version__= '0.1.1'
 def parse_args():
     argp = argparse.ArgumentParser(add_help=True, description='Check for OOM killer events', epilog='{0}: v.{1} by {2}'.format('check_pihole.py', __version__, __author__))
     argp.add_argument('-H', '--host', type=str, help="PiHole ip address or hostname")
-    argp.add_argument('-t', '--timeout', default=500, type=int, help='Timeout for request')
+    argp.add_argument('-t', '--timeout', default=10, type=int, help='Timeout for request. Default 10 seconds')
     args = argp.parse_args()
     if (args.host == None):
         argp.print_help()
@@ -20,15 +20,15 @@ def gtfo(exitcode, message=''):
         print(message)
     exit(exitcode)
 
-def check_pihole(host, timeout):
+def check_pihole(host, _timeout):
     status_url = 'http://' + host + '/admin/api.php?summaryRaw'
     try:
         request = urllib3.PoolManager()
-        content  = request.request('GET', status_url)
+        content  = request.request('GET', status_url, timeout=_timeout)
         decoded  = json.loads(content.data)
         return 0, decoded
     except Exception:
-        return 2, "Problems with accessing API"
+        return 2, "Problems with accessing API: Check if server is running."
 
 def main():
     args = parse_args()
