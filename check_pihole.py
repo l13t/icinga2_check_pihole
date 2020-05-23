7c5727cb197cecf158931ee2b573e976228ec339
Dmytro Prokhorenkov <liet@liet.kiev.ua>
2020-05-23 18:42:37 +0200

    Updated code style and added port
    * added flake8 config
    * removed unused functions
    * added port parameter in case pi-hole listen different port

--- check_pihole.py
+++ check_pihole.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python3
 
-import sys, urllib3, base64, re, argparse, json
+import argparse
+import json
+
+import urllib3
 
 __author__ = 'Dmytro Prokhorenkov'
-__version__= '0.1.2'
+__version__ = '0.2.0'
+
 
 def parse_args():
-    argp = argparse.ArgumentParser(add_help=True, description='Check Pi-hole status', epilog='{0}: v.{1} by {2}'.format('check_pihole.py', __version__, __author__))
-    argp.add_argument('-H', '--host', type=str, help="Pi-hole ip address or hostname")
-    argp.add_argument('-C', '--status_critical', dest='pihole_status', help="Forces CRITICAL when Pi-hole is disabled", action='store_true')
-    argp.add_argument('-W', '--status_warning', dest='pihole_status', help="Forces WARNING when Pi-hole is disabled", action='store_false')
+    argp = argparse.ArgumentParser(add_help=True,
+                                   description='Check Pi-hole status',
+                                   epilog='{0}: v.{1} by {2}'.format('check_pihole.py', __version__, __author__))
+    argp.add_argument('-H', '--host', type=str, help="Pi-hole ip address or hostname", required=True)
+    argp.add_argument('-P', '--port', type=int, help="Port number for Pi-Hole web UI", default=80)
+    argp.add_argument('-C', '--status_critical', dest='pihole_status',
+                      help="Forces CRITICAL when Pi-hole is disabled", action='store_true')
+    argp.add_argument('-W', '--status_warning', dest='pihole_status',
+                      help="Forces WARNING when Pi-hole is disabled", action='store_false')
     argp.add_argument('-t', '--timeout', default=10, type=int, help='Timeout for request. Default 10 seconds')
     argp.set_defaults(pihole_status=False)
     args = argp.parse_args()
-    if (args.host == None):
-        argp.print_help()
-        sys.exit(0)
     return args
+
 
 def gtfo(exitcode, message=''):
     if message:
         print(message)
     exit(exitcode)
 
-def check_pihole(host, _timeout):
-    status_url = 'http://' + host + '/admin/api.php?summaryRaw'
+
+def check_pihole(host, port, _timeout):
+    status_url = 'http://' + host + ('' if port == 80 else ":"+str(port)) + '/admin/api.php?summaryRaw'
     try:
         request = urllib3.PoolManager()
         content = request.request('GET', status_url, timeout=_timeout)
@@ -33,17 +41,22 @@ def check_pihole(host, _timeout):
     except Exception:
         return 2, "Problems with accessing API: Check if server is running."
 
+
 def main():
     args = parse_args()
-    exitcode, url_output = check_pihole(args.host, args.timeout)
+    exitcode, url_output = check_pihole(args.host, args.port, args.timeout)
     message = "OK: "
     if url_output["status"] != "enabled":
-        if args.pihole_status == True:
+        if args.pihole_status:
             message = "CRITICAL: "
         else:
             message = "WARNING: "
-    message = message + "Pi-hole is " + url_output["status"] + ": queries today - " + str(url_output["dns_queries_all_types"]) + ", domains blocked: " + str(url_output["ads_blocked_today"]) + ", percentage blocked: " + str(url_output["ads_percentage_today"]) + "|queries=" + str(url_output["dns_queries_all_types"]) +" blocked=" +  str(url_output["ads_blocked_today"])
+    message = message + "Pi-hole is " + url_output["status"] + ": queries today - " + \
+        str(url_output["dns_queries_all_types"]) + ", domains blocked: " + str(url_output["ads_blocked_today"]) + \
+        ", percentage blocked: " + str(url_output["ads_percentage_today"]) + \
+        "|queries=" + str(url_output["dns_queries_all_types"]) + " blocked=" + str(url_output["ads_blocked_today"])
     gtfo(exitcode, message)
+
 
 if __name__ == '__main__':
     main()
