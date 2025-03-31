79c6553a3a48b78c5e525924a2f91de39d50fe39
Dmytro Prokhorenkov <l13t@users.noreply.github.com>
2025-03-31 09:29:56 +0200

    fix: Updated get sid process and extended error output (#13)
    * Query session id with password
    * Updated getting sid section and improved output
    
    ---------
    
    Co-authored-by: Lukas Klass <spam.lukas.klass@gmail.com>

--- check_pihole.py
+++ check_pihole.py
@@ -40,9 +40,21 @@ def gtfo(exitcode, message=''):
 
 def check_pihole(host, port, auth, secure, _timeout, api):
     api_url = 'http' + ('s' if secure else '') + '://' + host + ('' if port == 80 else ":"+str(port)) + '/api/'
-    api_headers = {'accept': 'application/json', 'sid': auth}
+    cert_reqs = 'CERT_NONE' if secure else ''
     try:
-        cert_reqs = 'CERT_NONE' if secure else ''
+        request = urllib3.PoolManager(cert_reqs=cert_reqs)
+        content = request.request('POST', api_url + 'auth', body=json.dumps({"password": auth}), timeout=_timeout)
+        valid = json.loads(content.data.decode('utf8'))['session']['valid']
+        if not valid:
+            return 2, "Problems with accessing API: Check if provided password is correct."
+
+        sid = json.loads(content.data.decode('utf8'))['session']['sid']
+
+    except Exception:
+        return 2, "Problems with accessing API: Check if server is running."
+
+    api_headers = {'accept': 'application/json', 'sid': sid}
+    try:
         request = urllib3.PoolManager(cert_reqs=cert_reqs)
         content = request.request('GET', api_url + api, headers=api_headers, timeout=_timeout)
         decoded = json.loads(content.data.decode('utf8'))
